e14d209df018dc889b76d582586279253c2a325c
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-07-12 03:22:52 -0300

    feat(exercicios): solve third exercise

--- exercicios/03-rick-and-morty.js
+++ exercicios/03-rick-and-morty.js
@@ -1,4 +1,55 @@
 async function getRickAndMortyCharacters() {
-  //your code here...
+  async function getCharacterData(characterName) {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?name=${characterName}`
+      )
+      const data = await response.json()
+
+      if (data.results.length > 0) {
+        const character = data.results[0]
+        const { name, gender, image, species } = character
+
+        return {
+          nome: name,
+          genero: gender === 'Male' ? 'Homem' : 'Mulher',
+          avatar: image,
+          especie: species === 'Human' && 'Humano',
+        }
+      } else {
+        return null
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function fetchRickAndMortyCharacters() {
+    const characterNames = [
+      'Rick Sanchez',
+      'Morty Smith',
+      'Summer Smith',
+      'Beth Smith',
+      'Jerry Smith',
+    ]
+    const charactersDataList = []
+
+    for (const name of characterNames) {
+      const data = await getCharacterData(name)
+      if (data) {
+        charactersDataList.push(data)
+      }
+    }
+
+    return charactersDataList
+  }
+
+  fetchRickAndMortyCharacters().then((characters) => {
+    console.log(characters)
+    return characters
+  })
 }
+
+getRickAndMortyCharacters()
+
 module.exports = getRickAndMortyCharacters
