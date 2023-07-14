a4e41c3b59ed1b968094c626066e0ee4b6731544
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-07-14 08:29:54 -0300

    test: exercise 04 - uppercase

--- exercicios/04-lowercase.js
+++
@@ -1,6 +0,0 @@
-function checkIfTheFirstLetterIsUppercase(word) {
-  const firstLetter = word.charAt(0)
-  return firstLetter === firstLetter.toUpperCase()
-}
-
-module.exports = checkIfTheFirstLetterIsUppercase
